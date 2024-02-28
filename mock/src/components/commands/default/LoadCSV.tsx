836f7faef5a9d34b137ce783c265c93b1f34e4bc
asing122 <annika_singh@brown.edu>
2024-02-28 12:18:55 -0500

    feat: protected against going outside of protected directory

--- mock/src/components/commands/default/LoadCSV.tsx
+++ mock/src/components/commands/default/LoadCSV.tsx
@@ -18,12 +18,16 @@ export const loadCSV: REPLFunction = (
     .filter((val) => val != "" && val != " ")
     .map((v) => v.trim());
   if (loadFields.length == 2) {
-    var file = CSVmap.get(loadFields[1]) || [[]];
-    props.setFile(file);
-    if (file.length > 1) {
-      return "File successfully loaded.";
+    if (loadFields[1].startsWith("../")) {
+      return "Error: invalid load; unable to go outside protected directory";
     } else {
-      return "Error: unable to load file; invalid file path.";
+      var file = CSVmap.get(loadFields[1]) || [[]];
+      props.setFile(file);
+      if (file.length > 1) {
+        return "File successfully loaded.";
+      } else {
+        return "Error: unable to load file; invalid file path.";
+      }
     }
   }
   return "Error: invalid load; incorrect number of arguments.";
