e71d5c59ef583d5c80442c571ff821b6c5e11306
asing122 <annika_singh@brown.edu>
2024-03-02 08:25:00 -0500

    feat: added mocked search queries for tv.csv

--- mock/src/components/mocked/searchQueries.ts
+++ mock/src/components/mocked/searchQueries.ts
@@ -17,4 +17,9 @@ export const searchQueries: Map<string, string[][]> = new Map([
     "2,2004",
     [["Eternal Sunshine of the Spotless Mind", "Michel Gondry", "2004", "8.3"]],
   ],
+  ["TV show,Seinfeld", [["Seinfeld", "1989", "1998"]]],
+  ["TV show,Veep", [["Veep", "2012", "2019"]]],
+  ["TV show,Succession", [["Succession", "2018", "2023"]]],
+  ["TV show,Diplomat", [["Diplomat", "2023", "-"]]],
+  ["Year ended,-", [["Diplomat", "2023", "-"]]],
 ]);
