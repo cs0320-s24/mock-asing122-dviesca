e31d0b0b757a7f1167d18e0adca6af7f173905ce
asing122 <annika_singh@brown.edu>
2024-03-02 08:58:46 -0500

    feat: added new function for testing

---
+++ mock/src/components/commands/test-new-function/fileMovie.tsx
@@ -0,0 +1,36 @@
+import { Dispatch, SetStateAction } from "react";
+import { REPLFunction } from "../REPLFunction";
+import { CSVmap } from "../../mocked/CSVMap";
+
+/**
+ * REPLFunctionProps: generalized props for a function used in repl
+ * file: the CSV file being operated on
+ * modeBrief: a boolean indicating whether in brief or vermbose mode
+ * setModeBrief: function to update modeBrief
+ */
+interface REPLFunctionProps {
+  file: string[][];
+  setFile: Dispatch<SetStateAction<string[][]>>;
+  modeBrief: boolean;
+  setModeBrief: Dispatch<SetStateAction<boolean>>;
+}
+
+/**
+ * Shorthand for LoadCSV that loads movie.csv
+ * @param props REPLFunctionProps defined above
+ * @param args
+ * @returns a react component for the input block and submit button after a
+ *          load command is executed
+ */
+export const fileMovie: REPLFunction = (
+  props: REPLFunctionProps,
+  args: string[]
+): string => {
+  var file = CSVmap.get("movies.csv") || [[]];
+  props.setFile(file);
+  if (props.file.length > 1) {
+    return "File successfully loaded.";
+  } else {
+    return "Error: unable to load file.";
+  }
+};
