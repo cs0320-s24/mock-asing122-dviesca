00e480e513c382efb27afffc11035879433b57d2
asing122 <annika_singh@brown.edu>
2024-03-02 09:10:03 -0500

    fix: resolving merge conflict

--- mock/src/components/commands/default/SearchCSVByColName.tsx
+++ mock/src/components/commands/default/SearchCSVByColName.tsx
@@ -2,13 +2,6 @@ import { Dispatch, SetStateAction } from "react";
 import { REPLFunction } from "../REPLFunction";
 import { searchQueries } from "../../mocked/searchQueries";
 
-/**
- * REPLFunctionProps: generalized props for a function used in repl
- * file: the CSV file being operated on
- * setFile: function to update file
- * modeBrief: a boolean indicating whether in brief or vermbose mode
- * setModeBrief: function to update modeBrief
- */
 interface REPLFunctionProps {
   file: string[][];
   setFile: Dispatch<SetStateAction<string[][]>>;
@@ -16,15 +9,6 @@ interface REPLFunctionProps {
   setModeBrief: Dispatch<SetStateAction<boolean>>;
 }
 
-/**
- * searchCSVByColName Function
- *
- * Function to search CSV data by column name.
- *
- * @param props - The props object containing file, setFile, modeBrief, and setModeBrief.
- * @param args - The arguments passed to the function.
- * @returns A string representing the searched data or an error message.
- */
 export const searchCSVByColName: REPLFunction = (
   props: REPLFunctionProps,
   args: string[]
